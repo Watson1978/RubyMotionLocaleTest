df7a4edd8754d9f6fc6225b4f325b755ced9d53b
Watson <watson1978@gmail.com>
2013-07-07 22:26:56 +0900

    update

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,8 +1,5 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    # p NSLocale.preferredLanguages.objectAtIndex(0) # => ja
-    # p NSLocale.currentLocale.localeIdentifier # => ja_JP
-
     bundle = NSBundle.bundleWithPath(NSBundle.mainBundle.pathForResource("sample", ofType:"bundle"))
     NSLog bundle.localizedStringForKey("Hoge", value:nil, table:nil)
     true
